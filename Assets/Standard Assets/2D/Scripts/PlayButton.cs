c85eeae6354370191ed1f7262cd4f8dbcdc736a2
Michael Gilroy <Michael Gilroy>
2016-05-29 15:16:16 -0600

    Updated buttons

--- Assets/Standard Assets/2D/Scripts/PlayButton.cs
+++ Assets/Standard Assets/2D/Scripts/PlayButton.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class PlayButton : MonoBehaviour {
     public bool clicked = false;
     public GameObject menu;
+    public Transform rocketPrefab;
     //MenuManager mm;
     // Use this for initialization
     void Start () {
         //mm = GameObject.Find("MainCamera").GetComponent<MenuManager>();
-    }
+        //public Transform bulletPrefab;
+        Transform bullet = Instantiate(rocketPrefab) as Transform;
+        Physics2D.IgnoreCollision(rocketPrefab.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+}
 	
 	// Update is called once per frame
 	void Update () {
