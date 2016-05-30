3b78c13a80933be8fa3f3a07f61e2ecf8fd92e44
Michael Gilroy <Michael Gilroy>
2016-05-30 17:22:44 -0600

    Updated GUI fuel system, revised user controls

--- Assets/Standard Assets/2D/Scripts/PlayerController.cs
+++ Assets/Standard Assets/2D/Scripts/PlayerController.cs
@@ -2,24 +2,33 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    /*
+     _______  _____  _______ . _______      _______  _____  _______  ______ _     _ _______ _______ _______ _____
+    |  |  | |     | |  |  | ' |______      |______ |_____] |_____| |  ____ |_____| |______    |       |      |  
+    |  |  | |_____| |  |  |   ______|      ______| |       |     | |_____| |     | |______    |       |    __|__
+    
+    */
+
 
     public Transform target;
     bool menuMode = true;
-    float fuel = 5, maxfuel = 5;
+    float fuel = 100, maxfuel = 100;
     Rect fuelRect;
     Texture2D fuelTexture;
+    Camera cam;
 
     PlayButton play;
 
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, 3.0f, 0.0f);
-        fuelRect = new Rect(550.0f, 700.0f, 280.0f, 250.0f);
+        fuelRect = new Rect(20, 40, 80, 20);
         fuelTexture = new Texture2D(1, 1);
         fuelTexture.SetPixel(0, 0, Color.red);
         fuelTexture.Apply();
 
         play = GameObject.Find("play").GetComponent<PlayButton>();
+        cam = GameObject.Find("MainCamera").GetComponent<Camera>();
     }
     
     
@@ -28,14 +37,17 @@ public class PlayerController : MonoBehaviour {
         // when mouse is pressed and not in menu, get the distance & direction of click from rocket
         if (Input.GetMouseButton(0) && !menuMode && fuel >= 0.0f)
         {
-            Vector2 mouseClick = new Vector2(Input.mousePosition.x - 485.0f, Input.mousePosition.y - 165.0f) * -1;
+            Vector2 mouseClick = new Vector2(Input.mousePosition.x - cam.WorldToScreenPoint(transform.position).x, Input.mousePosition.y - cam.WorldToScreenPoint(transform.position).y) * -1;
+            print(Input.mousePosition);
+            print(transform.position.x + ", " + transform.position.y);
 
             GetComponent<Rigidbody2D>().AddForce(mouseClick);
 
             // get direction of click and set rocket direction away from it
             Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            //print(diff);
             diff.Normalize();
-            print(diff);
+            //print(diff);
 
             // a^2 + b^2 = c^2 for distance
             float rot_z = Mathf.Atan2(-diff.y, -diff.x) * Mathf.Rad2Deg;
@@ -57,9 +69,13 @@ public class PlayerController : MonoBehaviour {
 
     void OnGUI()
     {
-        float ratio = fuel / maxfuel;
-        float rectWidth = ratio * Screen.width / 3;
-        fuelRect.width = rectWidth;
-        GUI.DrawTexture(fuelRect, fuelTexture);
+        if (!menuMode)
+        {
+            float ratio = fuel / maxfuel;
+            float rectWidth = ratio * Screen.width / 3;
+            fuelRect.width = rectWidth;
+            GUI.DrawTexture(fuelRect, fuelTexture);
+        }
+        
     }
 }
